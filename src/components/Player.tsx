65b2dfbeb03d633459c421f67d7baef8bb37b3e1
Tarachand <tarachand90@gmail.com>
2025-02-16 14:44:41 +0530

    Add player name editing functionality with input field and edit/save toggle

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
+
 interface PlayerProps {
   name: string;
   symbol: string;
 }
 
 const Player = ({ name, symbol }: PlayerProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const editingMsg = !isEditing ? "Edit" : "Save";
+  const handleEditClick = () => {
+    setIsEditing(!isEditing);
+  };
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = <input type="text" required />;
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{editingMsg}</button>
     </li>
   );
 };
